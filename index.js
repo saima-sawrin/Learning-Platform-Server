ed3d58b88126020473b481836e8d7947d760df9c
Saima <saimasawrin324@gmail.com>
2022-10-24 23:18:10 +0600

    Courses json file added

--- index.js
+++ index.js
@@ -4,11 +4,21 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+const courses = require('./data/Courses.json')
 
 app.get('/', (req, res) => {
     res.send('Learning API Running');
 });
 
+app.get('/courses', (req, res) =>{
+    res.send(courses);
+});
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourses = courses.find(c => c._id === id);
+    res.send(selectedCourses);
+});
 
 app.listen(port, () => {
     console.log('Learning Platform running on port', port);
