a2f5dd5a6ab71f4459c375997c1a161905160f09
Saima <saimasawrin324@gmail.com>
2022-10-25 20:06:20 +0600

    Set vercel

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 const courses = require('./data/Courses.json')
+app.use(cors());
+const coursesInfo = require('./data/CourseInfo.json')
 
 app.get('/', (req, res) => {
     res.send('Learning API Running');
@@ -13,8 +15,16 @@ app.get('/', (req, res) => {
 app.get('/courses', (req, res) =>{
     res.send(courses);
 });
+app.get('/coursesInfo', (req, res) =>{
+    res.send(coursesInfo);
+});
 
 app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourses = courses.find(c => c._id === id);
+    res.send(selectedCourses);
+});
+app.get('/coursesInfo/:id', (req, res) => {
     const id = req.params.id;
     const selectedCourses = courses.find(c => c._id === id);
     res.send(selectedCourses);
