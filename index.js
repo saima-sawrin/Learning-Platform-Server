e330b16ec27d4a995749f39d4b350d57eeaeb5e9
Saima <saimasawrin324@gmail.com>
2022-10-26 15:49:24 +0600

    update json file

--- index.js
+++ index.js
@@ -1,34 +1,39 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
+const express = require('express')
+const app = express()
+const cors = require('cors')
 const port = process.env.PORT || 5000;
+app.use(cors())
 
-app.use(cors());
 const courses = require('./data/Courses.json')
-app.use(cors());
 const coursesInfo = require('./data/CourseInfo.json')
 
 app.get('/', (req, res) => {
-    res.send('Learning API Running');
+    res.send('Learning API Running..');
 });
 
 app.get('/courses', (req, res) =>{
     res.send(courses);
 });
+app.get('/courses/:id',(req,res)=>{
+    const id = req.params.id;
+    if(id === '07'){
+        res.send(coursesInfo);
+    }
+    else{
+      const course_category = coursesInfo.filter(c => c.course_id === id)
+      res.send(course_category)
+    }
+})
 app.get('/coursesInfo', (req, res) =>{
     res.send(coursesInfo);
 });
 
-app.get('/courses/:id', (req, res) => {
+app.get('/coursesInfo/:id',(req,res)=>{
     const id = req.params.id;
-    const selectedCourses = courses.find(c => c._id === id);
-    res.send(selectedCourses);
-});
-app.get('/coursesInfo/:id', (req, res) => {
-    const id = req.params.id;
-    const selectedCourses = courses.find(c => c._id === id);
-    res.send(selectedCourses);
-});
+    const selectedCourse = coursesInfo.find(c => c.id === id)
+    res.send(selectedCourse);
+    console.log(req.params.id)
+ })
 
 app.listen(port, () => {
     console.log('Learning Platform running on port', port);
